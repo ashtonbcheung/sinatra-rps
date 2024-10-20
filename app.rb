6711d689f4601cf513eb84b00bf708dbed3812b4
ashtonbcheung <70730294+ashtonbcheung@users.noreply.github.com>
2024-10-20 22:25:49 +0000

    complete

--- app.rb
+++ app.rb
@@ -6,13 +6,46 @@ get("/") do
 end
 
 get("/rock") do
-  ""
+  @computer_choice = ["rock", "paper", "scissors"].sample
+  @outcome = ""
+
+  if @computer_choice == "paper"
+    @outcome = "We lost!"
+  elsif @computer_choice == "scissors"
+    @outcome = "We win!"
+  else
+    @outcome = "We tied!"
+  end
+
+  erb(:rock)
 end
 
 get("/paper") do
-  ""
+  @computer_choice = ["rock", "paper", "scissors"].sample
+  @outcome = ""
+
+  if @computer_choice == "paper"
+    @outcome = "We tied!"
+  elsif @computer_choice == "scissors"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+
+  erb(:paper)
 end
 
 get("/scissors") do
-  ""
+  @computer_choice = ["rock", "paper", "scissors"].sample
+  @outcome = ""
+
+  if @computer_choice == "paper"
+    @outcome = "We won!"
+  elsif @computer_choice == "scissors"
+    @outcome = "We tied!"
+  else
+    @outcome = "We lost!"
+  end
+
+  erb(:scissors)
 end
